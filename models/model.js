9b766523d24f81468571a1dcddd07eaabe0c0f16
Hristina Staykova <hristina.staykova@gmail.com>
2019-12-19 20:31:13 +0100

    folders/files structure

---
+++ models/model.js
@@ -0,0 +1,7 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+var ormObject = {};
+
+// Export the database functions for the controller (catsController.js).
+module.exports = ormObject;
