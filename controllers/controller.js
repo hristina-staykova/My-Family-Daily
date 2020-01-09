b417bdac70a3c018b86fe2583467a234b5bef57b
Hristina Staykova <hristina.staykova@gmail.com>
2020-01-09 20:50:57 +0100

    db queries working

--- controllers/controller.js
+++ controllers/controller.js
@@ -24,6 +24,7 @@ router.get("/login", function(req, res) {
 });
 
 router.post("/api/login", function(req, res) {
+  //selectUser()
   //- if user exists
   //- if password is correct
   //- if user is admin
@@ -32,7 +33,7 @@ router.post("/api/login", function(req, res) {
 
 //create new user and enter the page
 router.post("/api/signup", function(req, res) {
-  //check the request - how do we pass the email & password in insertUser()
+  //check the request - how do we pass the email & password in insertUser(), check if the user exists - selectUser()
   orm.insertUser(req.body.user, cb);
   //what do we render on signup - main page?
   res.render("index");
@@ -40,7 +41,7 @@ router.post("/api/signup", function(req, res) {
 
 //adding new news
 router.post("/api/news", function(req, res) {
-  //check the request - how do we pass the data (req.body?)
+  //check the request - how do we pass the data (req.body?) - the news content and the logged user id
   orm.insertNews(news, user_id, cb);
   //we display the new news entry as a first entry?
   res.render("index");
@@ -48,7 +49,7 @@ router.post("/api/news", function(req, res) {
 
 //adding a comment to news
 router.post("/api/comments", function(req, res) {
-  //check the request - how do we pass the data (req.body?)
+  //check the request - how do we pass the data (req.body?) - the comment content and the logged user id
   orm.insertComment(comment, user_id, news_id, cb);
   res.render("index");
 });
@@ -58,22 +59,29 @@ router.get("/admin", function(req, res) {
   res.render("admin");
 });
 
-//admin page - get and display users from the db
+//admin page - get and display users from the db - working
 router.get("/admin/users", function(req, res) {
-  orm.selectUser(parameter1, parameter2);
-  res.render("admin");
+  console.log("admin users page");
+  orm.selectUsers(function(users) {
+    console.log(users);
+  });
+  //res.render("admin", {users});
 });
 
-//admin page - get and display news from the db
+//admin page - get and display news from the db - working
 router.get("/admin/news", function(req, res) {
-  orm.selectNews(parameter);
-  res.render("admin");
+  orm.selectNews(function(news) {
+    console.log(news);
+  });
+  //res.render("admin", {news});
 });
 
-//admin page - get and display comments from the db
+//admin page - get and display comments from the db - working
 router.get("/admin/comments", function(req, res) {
-  orm.selectComments(parameter);
-  res.render("admin");
+  orm.selectComments(function(comments) {
+    console.log(comments);
+  });
+  //res.render("admin", {comments});
 });
 
 //admin page - delete user from the db
@@ -90,18 +98,22 @@ router.put("/admin/users/:id", function(req, res) {
   res.render("admin");
 });
 
-//admin page - delete comment by id from the db
+//admin page - delete comment by id from the db - working
 router.delete("/admin/comments/:id", function(req, res) {
   var comment_id = req.params.id;
-  orm.deleteComment(comment_id, cb);
-  res.render("admin");
+  orm.deleteComment(comment_id, function(comments) {
+    console.log(comments);
+  });
+  //res.render("admin", {comments}); - what do we render after deleting a comment?
 });
 
-//admin page - delete news by id from the db + its comments
-router.delete("/admin/news/:id", function(req, res) {
+//admin page - delete news by id from the db + its comments - working
+router.get("/admin/news/:id", function(req, res) {
   var news_id = req.params.id;
-  orm.deleteNews(news_id, cb);
-  res.render("admin");
+  orm.deleteNews(news_id, function(news) {
+    console.log(news);
+  });
+  //res.render("admin", {news}); - what do we render after deleting news + its comments?
 });
 
 // Export routes for server.js to use.
