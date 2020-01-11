8b3f1b8abe5012373cd198b257019303d237f164
APaulaMiranda <apaulafmiranda@gmail.com>
2020-01-11 15:00:55 +0100

    update changes

--- controllers/controller.js
+++ controllers/controller.js
@@ -109,59 +109,32 @@ router.post("/api/comments", function (req, res) {
   );
 });
 
-<<<<<<< HEAD
-//on "/" we see the login page
+//on "/" we see the login page - OK
 router.get("/", function (req, res) {
   res.render("login");
 });
 
-//"index" is the main page after login/sign up - not fully working
-router.get("/index", function (req, res) {
-  news.selectRecentNews(function (recentNews) {
-    user.selectUser(1, function (user) {
-      console.log({ recentNews, user })
-      res.render("index", { recentNews, user })
-    })
-  })
-});
-
+//OK
 router.get("/signup", function (req, res) {
   res.render("signup");
 });
 
+//OK
 router.get("/login", function (req, res) {
   res.render("login");
 });
 
-router.post("/api/login", function (req, res) {
-=======
-//on "/" we see the login page - OK
-router.get("/", function(req, res) {
-  res.render("login");
-});
-
-//OK
-router.get("/signup", function(req, res) {
-  res.render("signup");
-});
-
-//OK
-router.get("/login", function(req, res) {
-  res.render("login");
-});
-
 //"index" is the main page after login/sign up - to be continued
-router.get("/index", function(req, res) {
-  news.selectRecentNews(10, function(recentNews) {
-    user.selectUser(1, function(user) {
+router.get("/index", function (req, res) {
+  news.selectRecentNews(10, function (recentNews) {
+    user.selectUser(1, function (user) {
       console.log({ recentNews, user });
       res.render("index", { recentNews, user });
     });
   });
 });
 
-router.post("/api/login", function(req, res) {
->>>>>>> a01c139e67ef811be0659a363235fd72abaf46a6
+router.post("/api/login", function (req, res) {
   //selectUser()
   //- if user exists
   //- if password is correct
