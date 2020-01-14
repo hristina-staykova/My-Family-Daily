26b1fed3bfe093408a2d6dafceb49a4b0e41b1c3
APaulaMiranda <apaulafmiranda@gmail.com>
2020-01-14 21:22:58 +0100

    added weather icon

--- public/assets/js/weather_api.js
+++ public/assets/js/weather_api.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     var weatherKey = "c42a7fcea0bcaffcf54bbbca1199417e";
     var weather = $("#weather");
 
+
     var resultsNum = 1;
     var queryURL =
         "https://api.openweathermap.org/data/2.5/weather?q=Berlin,de,de&appid=" +
@@ -15,8 +16,11 @@ $(document).ready(function () {
         })
         .then((myJson) => {
             weather.text(myJson.weather[0].description)
+            var imgURL = `http://openweathermap.org/img/wn/${myJson.weather[0].icon}@2x.png`
+            var img = $("<img>")
+            img.attr("src", imgURL)
             $(".weather-box").append(weather)
+            $(".weather-box").append(img)
             console.log(myJson.weather[0].description);
         });
 });
-
